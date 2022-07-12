69ad6c6914a2913d418ae75508ec4f32e4c2bfea
shekhar <Shekhargavali37@gmai.com>
2022-07-12 16:18:17 +0530

    changed

--- src/datattype/loops.java
+++ src/datattype/loops.java
@@ -3,7 +3,7 @@ package datattype;
 public class loops {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("this is my git hub comment ");
 
 	}
 
